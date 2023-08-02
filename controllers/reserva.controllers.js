8983a56dd1e34e38166a78fcc9febc0fb60dd20c
Lezcano Luciana <lezcanoceleste1972@gmail.com>
2023-08-02 16:18:35 -0300

    realice uno cambios en los controladores que estban mal

--- controllers/reserva.controllers.js
+++ controllers/reserva.controllers.js
@@ -16,42 +16,53 @@ ctrl.renderActualizarReserva = (req, res) => {
 // ==========================================
 
 // Obtener todas las reservas
-// Eliminar una reserva de forma lógica
-ctrl.eliminarReserva = async (req, res) => {
-  const { estado } = req.body;
+ctrl.ctrl.renderListaReservas = (req, res) => {
+  res.render("index");
+};
+// Formulario para crear una reserva
+ctrl.ctrl.renderFormNuevaReserva = (req, res) => {
+  res.render("nuevo");
+};
+
+// Formulario para editar una reserva
+ctrl.ctrl.renderFormEditarReserva = (req, res) => {
+  const { id } = req.params;
+  res.render("editar", { id });
+};
+
+// ==========================================
+//     Controladores para CRUD de reservas
+// ==========================================
+
+// Obtener todas las reservas
+ctrl.obtenerReservas = async (req, res) => {
   try {
-    const reserva = await Reserva.update(
-      {
-        estado: false,
+    const reservas = await Reserva.findAll({
+      where: {
+        estado: true,
       },
-      {
-        where: {
-          id: Number(req.params.id),
-        },
-      }
-    );
-    return res.json(reserva);
+    });
+    return res.render("index", {
+      reservas,
+    });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
-      message: "Error al actualizar la reserva",
+      message: "Error al obtener las reservas",
     });
   }
 };
-
-// Actualizar una reserva
-ctrl.actualizarReserva = async (req, res) => {
+// Obtener una reserva
+ctrl.obtenerReserva = async (req, res) => {
   try {
-    const { id } = req.params;
-    const reserva = await Reserva.findByPk(id);
-    await reserva.update(req.body);
-    return res.json({
-      message: "Reserva actualizada",
+    const reserva = await Reserva.findOne({
+      where: {
+        id: Number(req.params.id),
+      },
     });
+    return res.json(reserva);
   } catch (error) {
-    console.log("Error al actualizar la reserva", error);
-    return res.status(error.status || 500).json({
-      message: error.message,
+    return res.status(500).json({
+      message: "Error al obtener la reserva",
     });
   }
 };
@@ -90,43 +101,8 @@ ctrl.crearReserva = async (req, res) => {
     });
   }
 };
-// Obtener una reserva
-ctrl.obtenerReserva = async (req, res) => {
-  try {
-    const reserva = await Reserva.findOne({
-      where: {
-        id: Number(req.params.id),
-      },
-    });
-    return res.json(reserva);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error al obtener la reserva",
-    });
-  }
-};
-// ==========================================
-//     Controladores para CRUD de reservas
-// ==========================================
 
-// Obtener todas las reservas
-ctrl.obtenerReservas = async (req, res) => {
-  try {
-    const reservas = await Reserva.findAll({
-      where: {
-        estado: true,
-      },
-    });
-    return res.render("index", {
-      reservas,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error al obtener las reservas",
-    });
-  }
-};
-// Formulario para editar una reserva
+// Actualizar una reserva
 ctrl.actualizarReserva = async (req, res) => {
   try {
     const { id } = req.params;
@@ -142,38 +118,25 @@ ctrl.actualizarReserva = async (req, res) => {
     });
   }
 };
-
-// Formulario para crear una reserva
-ctrl.crearReserva = async (req, res) => {
-  const {
-    codigo,
-    pelicula_nombre,
-    nombre,
-    apellido,
-    fecha_pelicula,
-    entrada_hora,
-    salida_hora,
-    asiento,
-    precio,
-  } = req.body;
-
+// Eliminar una reserva de forma lógica
+ctrl.eliminarReserva = async (req, res) => {
+  const { estado } = req.body;
   try {
-    const reserva = await Reserva.create({
-      codigo: new Date().getTime(),
-      pelicula_nombre,
-      nombre,
-      apellido,
-      fecha_pelicula,
-      entrada_hora,
-      salida_hora,
-      asiento,
-      precio,
-    });
+    const reserva = await Reserva.update(
+      {
+        estado: false,
+      },
+      {
+        where: {
+          id: Number(req.params.id),
+        },
+      }
+    );
     return res.json(reserva);
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message: "Error al crear la reserva",
+      message: "Error al actualizar la reserva",
     });
   }
 };
