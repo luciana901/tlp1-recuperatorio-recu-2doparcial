5bf38c811afd6bc923913a30481c5230e60b6c59
Lezcano Luciana <lezcanoceleste1972@gmail.com>
2023-08-02 14:44:42 -0300

    primer commit

---
+++ controllers/reserva.controllers.js
@@ -0,0 +1,36 @@
+// TODO: Crear controladores para cada una de las rutas de reserva.
+const ctrl = {};
+
+// ==========================================
+//    Controladores para renderizar vistas
+// ==========================================
+
+// Obtener todas las reservas
+ctrl.
+// Formulario para crear una reserva
+ctrl.
+
+// Formulario para editar una reserva
+ctrl.
+
+// ==========================================
+//     Controladores para CRUD de reservas
+// ==========================================
+
+// Obtener todas las reservas
+ctrl.
+
+// Obtener una reserva
+ctrl.
+
+// Crear una reserva
+ctrl.
+
+// Actualizar una reserva
+ctrl.
+
+// Eliminar una reserva de forma lógica
+ctrl.
+
+
+module.exports = ctrl;
---
+++ routes/reserva.routes.js
@@ -0,0 +1,36 @@
+// TODO: Importar el modelo y controladores de reservas, luego vincular rutas con controladores
+
+const router = require('express').Router();
+
+
+// ==========================================
+//         Rutas para renderizar vistas
+// ==========================================
+
+// Vista para todas las reservas
+router.get('/lista-reservas', )
+
+// Formulario para crear una reserva
+router.get('/nueva-reserva', )
+
+// Formulario para editar una reserva
+router.get('/editar-reserva/:id', )
+
+// ==========================================
+//         Rutas para CRUD de reservas
+// ==========================================
+
+// Obtener todas las reservas
+router.get('/api/',);
+ 
+// Crear una reserva
+router.post('/api/',);
+ 
+// Actualizar una reserva
+router.put('/api/:id',);
+ 
+// Eliminar una reserva de forma lógica
+router.delete('/api/:id',);
+
+ 
+ module.exports = router;
